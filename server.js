1931e56b8db5a572abf0b6a0430610a53159b1fc
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-17 13:07:14 +0800

    change default apollo server landing page

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const { typeDefs } = require('./Schema/TypeDefs');
 const { resolvers } = require('./Schema/Resolvers');
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require('apollo-server-core');
 
 const PORT = process.env.PORT || 4000;
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
 
   await server.start();
 
