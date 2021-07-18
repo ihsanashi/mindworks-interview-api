d373a697951daf02eb42b79a3b11360fd1092e99
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-18 13:46:37 +0800

    add sorting, pagination limit

--- Schema/Resolvers.js
+++ Schema/Resolvers.js
@@ -1,22 +1,84 @@
 const axios = require('axios');
 
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
 const resolvers = {
   Query: {
-    posts() {
-      return axios
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then((res) => res.data);
-    },
-    post(parent, args) {
+    post: (parent, args) => {
       const { id } = args;
-      return axios
-        .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        .then((res) => res.data);
+      return axios.get(`${baseUrl}/posts/${id}`).then((res) => res.data);
     },
-    comments() {
-      return axios
-        .get(`https://jsonplaceholder.typicode.com/comments`)
-        .then((res) => res.data);
+    posts: async (parent, args) => {
+      const { sortBy, limit } = args;
+
+      if (sortBy && limit) {
+        const { order } = sortBy;
+
+        const result = await axios
+          .get(`${baseUrl}/posts`)
+          .then((res) => res.data);
+
+        const sorted = order === 'ASC' ? [...result] : [...result].reverse();
+
+        return sorted.slice(0, limit);
+      }
+
+      if (sortBy) {
+        const { order } = sortBy;
+
+        const result = await axios
+          .get(`${baseUrl}/posts`)
+          .then((res) => res.data);
+
+        const sorted = order === 'ASC' ? [...result] : [...result].reverse();
+
+        return sorted;
+      }
+
+      if (limit) {
+        const result = await axios
+          .get(`${baseUrl}/posts`)
+          .then((res) => res.data);
+        return result.slice(0, limit);
+      }
+
+      return axios.get(`${baseUrl}/posts`).then((res) => res.data);
+    },
+    comments: async (parent, args) => {
+      const { sortBy, limit } = args;
+
+      if (sortBy && limit) {
+        const { order } = sortBy;
+
+        const result = await axios
+          .get(`${baseUrl}/comments`)
+          .then((res) => res.data);
+
+        const sorted = order === 'ASC' ? [...result] : [...result].reverse();
+
+        return sorted.slice(0, limit);
+      }
+
+      if (sortBy) {
+        const { order } = sortBy;
+
+        const result = await axios
+          .get(`${baseUrl}/comments`)
+          .then((res) => res.data);
+
+        const sorted = order === 'ASC' ? [...result] : [...result].reverse();
+
+        return sorted;
+      }
+
+      if (limit) {
+        const result = await axios
+          .get(`${baseUrl}/comments`)
+          .then((res) => res.data);
+        return result.slice(0, limit);
+      }
+
+      return axios.get(`${baseUrl}/comments`).then((res) => res.data);
     },
   },
 };
--- Schema/TypeDefs.js
+++ Schema/TypeDefs.js
@@ -2,25 +2,50 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type Post {
-    userId: Int!
-    id: Int!
+    userId: ID!
+    id: ID!
     title: String!
     body: String!
   }
 
   type Comment {
-    postId: Int!
-    id: Int!
+    postId: ID!
+    id: ID!
     name: String!
     email: String!
     body: String!
   }
 
+  enum PostSortField {
+    id
+    # title
+  }
+
+  enum CommentSortField {
+    id
+    # name
+  }
+
+  enum Order {
+    ASC
+    DESC
+  }
+
+  input CommentSort {
+    field: CommentSortField!
+    order: Order!
+  }
+
+  input PostSort {
+    field: PostSortField!
+    order: Order!
+  }
+
   # Queries
   type Query {
     post(id: Int!): Post!
-    posts: [Post!]!
-    comments(name: String, email: String, body: String): [Comment!]!
+    posts(sortBy: PostSort, limit: Int): [Post!]!
+    comments(sortBy: CommentSort, limit: Int): [Comment!]!
   }
 `;
 
