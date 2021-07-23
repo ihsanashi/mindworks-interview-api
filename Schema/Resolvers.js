a8faf345d9e009c63a524f5339339de0a8fc422d
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-23 14:37:34 +0800

    allow comments to be filtered with query strings

--- Schema/Resolvers.js
+++ Schema/Resolvers.js
@@ -45,26 +45,63 @@ const resolvers = {
       return axios.get(`${baseUrl}/posts`).then((res) => res.data);
     },
     comments: async (parent, args) => {
-      const { sortBy, limit } = args;
+      const { sortBy, limit, where } = args;
 
-      if (sortBy && limit) {
+      let result = await axios
+        .get(`${baseUrl}/comments`)
+        .then((res) => res.data);
+
+      if (sortBy && limit && where) {
         const { order } = sortBy;
 
-        const result = await axios
-          .get(`${baseUrl}/comments`)
-          .then((res) => res.data);
+        for (const [key] of Object.entries(where)) {
+          result = result.filter((comment) => {
+            if (comment[key].includes(where[key])) return true;
+            return false;
+          });
+        }
 
         const sorted = order === 'ASC' ? [...result] : [...result].reverse();
 
         return sorted.slice(0, limit);
       }
 
-      if (sortBy) {
+      if (sortBy && limit) {
         const { order } = sortBy;
 
-        const result = await axios
-          .get(`${baseUrl}/comments`)
-          .then((res) => res.data);
+        const sorted = order === 'ASC' ? [...result] : [...result].reverse();
+
+        return sorted.slice(0, limit);
+      }
+
+      if (sortBy && where) {
+        const { order } = sortBy;
+
+        for (const [key] of Object.entries(where)) {
+          result = result.filter((comment) => {
+            if (comment[key].includes(where[key])) return true;
+            return false;
+          });
+        }
+
+        const sorted = order === 'ASC' ? [...result] : [...result].reverse();
+
+        return sorted;
+      }
+
+      if (limit && where) {
+        for (const [key] of Object.entries(where)) {
+          result = result.filter((comment) => {
+            if (comment[key].includes(where[key])) return true;
+            return false;
+          });
+        }
+
+        return result.slice(0, limit);
+      }
+
+      if (sortBy) {
+        const { order } = sortBy;
 
         const sorted = order === 'ASC' ? [...result] : [...result].reverse();
 
@@ -72,13 +109,21 @@ const resolvers = {
       }
 
       if (limit) {
-        const result = await axios
-          .get(`${baseUrl}/comments`)
-          .then((res) => res.data);
         return result.slice(0, limit);
       }
 
-      return axios.get(`${baseUrl}/comments`).then((res) => res.data);
+      if (where) {
+        for (const [key] of Object.entries(where)) {
+          result = result.filter((comment) => {
+            if (comment[key].includes(where[key])) return true;
+            return false;
+          });
+        }
+
+        return result;
+      }
+
+      return result;
     },
   },
 };
